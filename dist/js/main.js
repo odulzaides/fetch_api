c6c4623f388d1bb791724e0b34a73b89c803964d
odulzaides <dulzaides.oscar@gmail.com>
2021-09-11 10:17:44 -0700

    FEATURE COMPLETE: Dropdown searches state upon selection.

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,34 +16,37 @@ statesList.forEach(element => {
     // selected state to search for 
 var searchState;
 
-// Fetch data based on selection
-document.getElementById('search').addEventListener('click',
-        function(e) {
-            e.preventDefault();
-            searchState = document.getElementById('state').value;
+// Fetch data based on dropdown selection
+document.getElementById('statesList').addEventListener('click',
+    function(e) {
+        e.preventDefault();
+        // let statesList = document.getElementById('statesList');
+        // searchState = statesList.
+        searchState = document.getElementById('statesList').value;
 
-            // setup url to search a state
-            let dataSearch = `https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/${searchState}/president.json`
-            console.log(dataSearch);
-            let promise = fetch(dataSearch);
 
-            promise.then(response => {
-                if (response.status !== 200) {
-                    let data = response.status;
+        // setup url to search a state
+        let dataSearch = `https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/${searchState}/president.json`
+        console.log(dataSearch);
+        let promise = fetch(dataSearch);
 
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
+        promise.then(response => {
+            if (response.status !== 200) {
+                let data = response.status;
 
-                    return;
-                }
-                response.json().then(data => {
-                    // console.log(data);
-                    let candidates = data.data.races[0].candidates;
-                    let output = '<th>Candidate</th><th>%</th><th>Absentee Votes</th><th>Absentee  Percent</th>';
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
 
-                    candidates.forEach(element => {
-                            // console.log(element);
-                            output += `                
+                return;
+            }
+            response.json().then(data => {
+                // console.log(data);
+                let candidates = data.data.races[0].candidates;
+                let output = '<th>Candidate</th><th>%</th><th>Absentee Votes</th><th>Absentee  Percent</th>';
+
+                candidates.forEach(element => {
+                        // console.log(element);
+                        output += `                
                         <tr>
                             <td>${element.name_display}</td>
                             <td>${element.percent_display}%</td>   
@@ -51,14 +54,13 @@ document.getElementById('search').addEventListener('click',
                             <td>${element.absentee_percent}</td>
                         </tr>
                     `
-                        })
-                        // console.log(output);
-                    document.getElementById('main').innerHTML = output
-                        // console.log(candidates)
-                }).catch(error => {
-                    console.log(error.message);
-                })
-            });
-        }
-    )
-    // alert("HELLO" + searchState);
+                    })
+                    // console.log(output);
+                document.getElementById('main').innerHTML = output
+                    // console.log(candidates)
+            }).catch(error => {
+                console.log(error.message);
+            })
+        });
+    }
+)
